541574113efbb7272566562a29aa6a385910db26
E1257010 Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2026-04-01 21:01:23 -0300

    Introdução a FastApi

---
+++ curso-ia/projeto/app/endpoint.py
@@ -0,0 +1,24 @@
+import json
+from http import HTTPStatus
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+from starlette.responses import Response
+
+router = APIRouter()
+
+
+class EventsSchema(BaseModel):
+    event_id: str
+    event_type: str
+    event_data: dict
+
+
+@router.post("/", dependencies=[])
+def handle_event(data: EventsSchema) -> Response:
+    print(data)
+
+    return Response(
+        content=json.dumps({"message": "Data received"}),
+        status_code=HTTPStatus.ACCEPTED,
+    )
